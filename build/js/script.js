a55fa92a41c026a9ef72e469864f4046ffe1d331
dariaprikhodko <favour313@mail.ru>
2019-08-17 22:47:54 +0900

    сверстала слайдер

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+import { tns } from "./node_modules/tiny-slider/src/tiny-slider"
 
 var buttonCross = document.querySelectorAll('.button-cross');
 var answer = document.querySelectorAll('.faq-list__answer');
@@ -31,3 +32,17 @@ function handleGoToFeatures() {
   features.scrollIntoView({block: 'start', behavior: 'smooth'});
 }
 buttonInfo.addEventListener('click', handleGoToFeatures);
+
+const slider = tns({
+    container: '.slider',
+    loop: true,
+    items: 1,
+    slideBy: 'page',
+    nav: false,    
+    autoplay: true,
+    speed: 400,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    lazyload: true,
+    controlsContainer: "#customize-controls",
+  });
