54065bee146920f798d45f77f16b728f20acf777
dariaprikhodko <favour313@mail.ru>
2019-08-20 22:56:09 +0900

    логика слайда

--- source/js/script.js
+++ source/js/script.js
@@ -7,7 +7,14 @@
   var projects = document.querySelector('.projects__title');
   var buttonInfo = document.getElementById('moreInfo');
   var features = document.querySelector('.features');
-
+  var buttonMonstroy = document.getElementById('montstroy');
+  var montstroySlider = document.getElementById('montstroy-slider');
+  var buttonSmart = document.getElementById('smart-device');
+  var smartSlider = document.getElementById('smart-slider');
+  var buttonBike = document.getElementById('bikeshop');
+  var bikeSlider = document.getElementById('bike-slider');
+  var buttonEscape = document.getElementById('escapeRoom');
+  var escapeSlider = document.getElementById('escape-slider');
 
   buttonCross.forEach(function (btn, i) {
     btn.addEventListener('click', function () {
@@ -33,23 +40,52 @@
   }
   buttonInfo.addEventListener('click', handleGoToFeatures);
 
-  // var mySwiper = new Swiper('.gallery-top', {
-  //   spaceBetween: 10,
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  // });
+  // slider show
 
-  // var thumbs = document.querySelectorAll('.gallery-thumbs__thumb');
-  // thumbs.forEach(function (el, i) {
-  //   el.addEventListener('click', function () {
-  //     mySwiper.slideTo(i, 500);
-  //   });
-  // });
+  buttonMonstroy.addEventListener('click', function () {
+
+    if (montstroySlider.classList.contains('visually-hidden')) {
+      montstroySlider.classList.remove('visually-hidden');
+    }
+  });
+
+  buttonSmart.addEventListener('click', function () {
+
+    if (smartSlider.classList.contains('visually-hidden')) {
+      smartSlider.classList.remove('visually-hidden');
+    }
+  });
+
+  buttonBike.addEventListener('click', function () {
+
+    if (bikeSlider.classList.contains('visually-hidden')) {
+      bikeSlider.classList.remove('visually-hidden');
+    }
+  });
+
+  buttonEscape.addEventListener('click', function () {
+
+    if (escapeSlider.classList.contains('visually-hidden')) {
+      escapeSlider.classList.remove('visually-hidden');
+    }
+  });
 
 })();
 
+// var mySwiper = new Swiper('.gallery-top', {
+//   spaceBetween: 10,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+// var thumbs = document.querySelectorAll('.gallery-thumbs__thumb');
+// thumbs.forEach(function (el, i) {
+//   el.addEventListener('click', function () {
+//     mySwiper.slideTo(i, 500);
+//   });
+// });
 
 // buttonCross.forEach(function (btn, i) {
 //   btn.addEventListener('click', function () {
